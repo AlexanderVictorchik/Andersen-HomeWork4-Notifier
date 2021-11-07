f802c3b5c27f85ad104ec5f64aa7bdc38bcfb6dc
Deo <lukyanenko88@gmail.com>
2021-11-07 21:38:47 +0200

    import fixed

--- src/main/java/Notifier.java
+++ src/main/java/Notifier.java
@@ -1,10 +1,14 @@
+import command.CommandService;
+
 public class Notifier {
 
     public static void main(String[] args) {
 
 
-        // TODO: 07.11.2021 uncomment for test
-        //CommandService.getAll();
+        // TODO: 07.11.2021 uncomment for test 
+        
+
+         CommandService.getAll();
         //CommandService.getByGroup("green");
         //CommandService.getByRole("admin");
         //CommandService.getByRoleAndGroup("user", "green");
