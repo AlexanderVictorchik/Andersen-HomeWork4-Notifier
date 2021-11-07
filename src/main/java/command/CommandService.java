eb7b887b0c8b099a3b1140d5062584e993d0cb50
Deo <lukyanenko88@gmail.com>
2021-11-07 19:39:25 +0200

    better working example

--- src/main/java/command/CommandService.java
+++ src/main/java/command/CommandService.java
@@ -3,20 +3,19 @@ package command;
 import java.util.List;
 
 public class CommandService {
-    public static void main(String[] args) {
-        UserService userService = new UserServiceImplementationService()
-                .getPort(UserService.class);
+        public static List<User>getAll() {
+            UserService userService = new UserServiceImplementationService()
+                    .getPort(UserService.class);
 
 
-
-        List <User> users= userService.findAll();
-        System.out.println(users.get(0).getUsername());
-
-
-
-
-    }
-
+            List<User> users = userService.findAll();
+            System.out.println(users.get(0).getUsername());
+            System.out.println(users.get(0).getFirstName());
+            System.out.println(users.get(0).getLastName());
+            System.out.println(users.get(0).getRole());
+            System.out.println(users.get(0).getGroup());
+return users;
+        }
 
 
 
